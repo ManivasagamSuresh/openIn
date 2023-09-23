f29773c6c239303179bb722377427decfdd4bf75
Manivasagam Suresh <manivasagam.suresh12@gmail.com>
2023-09-23 19:42:55 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
 module.exports = {
-  images: {
-    domains: ['lh3.googleusercontent.com'], // Add the desired domains here
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Exclude styled-jsx from being bundled
-    config.externals.push('styled-jsx');
-    
-    return config;
-  },
-};
+    images: {
+      domains: ['lh3.googleusercontent.com'], // Add the desired domains here
+    },
+  };
