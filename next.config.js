e2615b90c0c6f2fb57f32fb81b10e3fd9aab0a46
Manivasagam Suresh <manivasagam.suresh12@gmail.com>
2023-09-23 19:49:58 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 module.exports = {
-    images: {
-      domains: ['lh3.googleusercontent.com'], // Add the desired domains here
-    },
-  };
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Add the desired domains here
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Exclude styled-jsx from being bundled
+    if (!isServer) {
+      config.resolve.alias['styled-jsx$'] = require.resolve('styled-jsx/noop.js');
+    }
+    return config;
+  },
+};
