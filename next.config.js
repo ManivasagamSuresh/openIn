33913c52af80dde69c260b7f85d09592cf8a3666
Manivasagam Suresh <manivasagam.suresh12@gmail.com>
2023-09-23 21:00:29 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['lh3.googleusercontent.com'], // Add the desired domains here
   },
